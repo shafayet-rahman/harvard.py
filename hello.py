43c6df9c2efea03a3740264e85c63d66d4b3fb7d
meran-ahsan <chiamahiru@gmail.com>
2024-12-10 13:19:50 +0600

    update2

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-name = input("what's your name?");
-print(name);
+name = input("what's your name?");#asking user for their name and saving it in a variable named "name"
+print(name);#the user input print statement 
 #sahira pocha
+'''
+gugugaga
+'''
