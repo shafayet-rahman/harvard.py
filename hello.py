dfc1484f9e9925652bdf57ce1a7cac11960c09ec
meran-ahsan <chiamahiru@gmail.com>
2024-12-12 09:48:01 +0600

    update4

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-name = input("what's your name?");#asking user for their name and saving it in a variable named "name"
-print(name);#the user input print statement 
+'''name = input("what's your name?").strip().title();#asking user for their name and saving it in a variable named "name"
+print(f"hello, {name}");'''#the user input print statement 
 #sahira pocha
+
+#print has an auto escape character you can re write it like this
 '''
-gugugaga
-sahira
+print("gugugaga ", end ="")
+print("sahiru") #this will print everything in a single line of code
 '''
