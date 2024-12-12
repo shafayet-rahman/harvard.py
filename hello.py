4839e4cbd126b9725fc461cef034e7fb5c8fe9a9
meran-ahsan <chiamahiru@gmail.com>
2024-12-12 10:01:14 +0600

    update6

--- hello.py
+++ hello.py
@@ -10,7 +10,11 @@ print("sahiru") #this will print everything in a single line of code
 #python string methods are pretty cool - lower(),upper(),title(),strip(),replace(),etc
 
 #split users name into two variables
-name = input("whatcha name lil vru? ")
+
+'''name = input("whatcha name lil vru? ")
 first, last = name.split(" ")
 print(first)
-print("Oh wait forgot your last name lil vru lemme cook" "\n" + last)
+print("Oh wait forgot your last name lil vru lemme cook" "\n" + last)'''
+#type conversion any input taken from the user comes as text even numbers u have to use int to convert it into num first
+'''z = 1000000
+print(f"{z:,}")'''#numeric formatting syntax
